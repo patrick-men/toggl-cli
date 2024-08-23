a5124a2fe06bbec1e3627f7531d0c2f40d74ae48
Patrick Palma Mendes <patrick-mendes@bluewin.ch>
2024-08-23 23:36:52 +0200

    fix: time now works correctly

--- toggl-sketchybar.sh
+++ toggl-sketchybar.sh
@@ -20,16 +20,26 @@ if [[ -z "$DESCRIPTION" ]]; then
 fi
 
 # running time calc
-json_running_time=$(echo $current_entry | jq -r '.duration')
+
+json_start_time=$(echo $entry | jq -r '.start')
+        
+# start time formatting
+json_start_time=$(echo $current_entry | jq -r '.start')
+json_start_time=${json_start_time%+00:00}  # remove +00:00
+json_start_time_unix=$(date -j -u -f "%Y-%m-%dT%H:%M:%S" "$json_start_time" +"%s")
+
 current_time_unix=$(date +%s)
+running_time_seconds=$((current_time_unix - json_start_time_unix))
 
-running_time_seconds=$((current_time_unix + json_running_time))
+if (( running_time_seconds < 60 )); then
+    TIME_RUNNING="0d 0h 0m"
+else
+    days=$((running_time_seconds/86400))
+    hours=$((running_time_seconds/3600%24))
+    minutes=$((running_time_seconds/60%60))
 
-days=$((running_time_seconds/86400))
-hours=$((running_time_seconds/3600%24))
-minutes=$((running_time_seconds/60%60))
-
-TIME_RUNNING="${days}d ${hours}h ${minutes}m"
+    TIME_RUNNING="${days}d ${hours}h ${minutes}m"
+fi
 
 output="$(printf "%s - %s" "$DESCRIPTION" "$TIME_RUNNING" | xargs)"
 
