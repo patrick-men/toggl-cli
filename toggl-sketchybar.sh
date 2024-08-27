5bc7880f171a74ce2684a62c1c23c8685f50bbbc
Patrick Palma Mendes <patrick-mendes@bluewin.ch>
2024-08-27 13:05:10 +0200

    fix: sketchybar now uses relative script dirs

--- toggl-sketchybar.sh
+++ toggl-sketchybar.sh
@@ -2,21 +2,22 @@
 
 #TODO: handle case where no task is ongoing
 #TODO: split into 3 > one with current task that stops task on click, one that starts task type X and one that starts task type Y
+script_dir="$(dirname "$(realpath "$0")")"
 
-
-source funcs.sh
-source constants.sh
+source "$script_dir/funcs.sh"
+source "$script_dir/constants.sh"
 
 NAME="toggl"
 
-current_entry=$(curl -s -u $(cat $credentials_file | base64 -d) $current_entry_url)
-
+current_entry=$(curl -s -v -u $(cat "$credentials_file" | base64 -d) $current_entry_url 2>> ~/.sketchlog)
 if [[ $current_entry == "null" ]]; then
     red_text "No current time entry found."
     exit 1
 fi
 
+
 DESCRIPTION=$(echo $current_entry | jq -r '.description')
+
 
 # Check if TASK is empty
 if [[ -z "$DESCRIPTION" ]]; then
@@ -25,15 +26,16 @@ fi
 
 # running time calc
 
-json_start_time=$(echo $entry | jq -r '.start')
-        
-# start time formatting
 json_start_time=$(echo $current_entry | jq -r '.start')
+        
+
+# start time formatting
 json_start_time=${json_start_time%+00:00}  # remove +00:00
 json_start_time_unix=$(date -j -u -f "%Y-%m-%dT%H:%M:%S" "$json_start_time" +"%s")
 
 current_time_unix=$(date +%s)
 running_time_seconds=$((current_time_unix - json_start_time_unix))
+
 
 if (( running_time_seconds < 60 )); then
     TIME_RUNNING="0d 0h 0m"
@@ -46,7 +48,5 @@ else
 fi
 
 output="$(printf "%s - %s" "$DESCRIPTION" "$TIME_RUNNING" | xargs)"
-
-echo $output
 
 sketchybar -m --set $NAME label="$output"
